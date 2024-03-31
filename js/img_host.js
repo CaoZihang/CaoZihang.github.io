ee3d24ac29be48e50a9c472fa0d5e6896a17488d
CaoZihang <cao_zihang@126.com>
2024-03-31 15:18:46 +0800

    debug

--- js/img_host.js
+++ js/img_host.js
@@ -1,14 +1,14 @@
 // 修改云服务中博客镜像的图床为云服务器图床镜像
 window.onload = function() {
-    console.log("[[[[[[[[");
+    // console.log("[[[[[[[[");
     let domain = window.location.hostname;
-    console.log(domain);
-    if (domain = "blog.caozihang.com") {
+    // console.log(domain);
+    if (domain != "www.caozihang.com") {
         let imgs = document.querySelectorAll('img');
         const regex = /https:\/\/img.caozihang.com\/img\//;
         const replacement = "http://cnimg.caozihang.com/";
 
-        console.log(imgs);
+        // console.log(imgs);
         for (let i = 0; i < imgs.length; i++) {
             console.log(imgs[i].src);
             if (regex.test(imgs[i].src)) {
@@ -17,6 +17,3 @@ window.onload = function() {
         }
     }
 }
-
-}
-
